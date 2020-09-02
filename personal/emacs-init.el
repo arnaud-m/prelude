b521d564eddbb2e257aa27088c1bd9a607f5e3e7
Arnaud Malapert <arnaud.malapert@univ-cotedazur.fr>
2020-09-02 15:55:42 +0200

    Improve the loading of org configuration files

--- personal/emacs-init.el
+++ personal/emacs-init.el
@@ -1,8 +1,16 @@
 (require 'org)
-(org-babel-load-file
- (expand-file-name "perso-emacs-init.org" "~/.emacs.d/org")
- )
 
-(org-babel-load-file
- (expand-file-name "perso-dired+.org" "~/.emacs.d/org")
- )
+;; Here, we mimic the init.el file
+;; to load personal configuration files written with org-mode
+
+(defvar prelude-perso-org-dir (expand-file-name "org" prelude-dir))
+(defvar prelude-perso-org-files '("perso-emacs-init.org" "perso-dired+.org"))
+
+(defun prelude-perso-org-babel-load-files (filenames)
+  "Load literate configuration files written with org-mode"
+  (dolist (f filenames)
+    (org-babel-load-file
+     (expand-file-name f prelude-perso-org-dir)
+     )))
+
+(prelude-perso-org-babel-load-files prelude-perso-org-files)
